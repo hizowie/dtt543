f77056cacd94e0bc49685c3cb59f8c8d777ba020
howie <howie.catlin@gmail.com>
2017-10-28 16:34:07 -0700

    asdf

---
+++ src/HW2.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : HW2.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    //input:
+        //[0]: int port; required
+        //[1]: string machineName; required
+        //[2]: int testID; optional
+        //[3]: int windowSize; optional
+        //[4]: int timeoutLength; optional
+
+
+
+
+
+}
