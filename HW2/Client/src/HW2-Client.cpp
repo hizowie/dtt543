4de256a944f1a2d9697a786294083962c1c9a97a
howie <howie.catlin@gmail.com>
2017-12-09 19:07:45 -0800

    HW2 setup to test prototype TCP Delayed ACK

---
+++ HW2/Client/src/HW2-Client.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HW2-Client.cpp
+// Author      : Howie Catlin
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
---
+++ HW2/Server/src/HW2-Server.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : HW2-Server.cpp
+// Author      : Howie Catlin
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+    int seqNum = -1;
+    int MAX_BUF = 1460;
+    int MAX_SEQ = 10000;
+    int timeout = 5000;
+    char ack[MAX_BUF];
+
+
+    vector<int> buf;
+    vector<int>::iterator it;
+
+
+
+}
